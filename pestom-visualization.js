5d100132606619179e5adf899db5eeacfc2152f9
Bastian Bechtold <bastian.bechtold@jade-hs.de>
2016-10-11 17:32:46 +0200

    adds illustration for PESTO-M

---
+++ pestom-visualization.js
@@ -0,0 +1,43 @@
+var pestomdata = null;
+fetch('pestomdata.json', {method:'GET'}).then(response => {
+    response.json().then(data => {
+        pestomdata = data;
+        var f0 = document.getElementById('PESTO-M:f0:slider').value;
+        updatePESTOM(f0);
+        document.getElementById('PESTO-M:f0:value').innerHTML = f0 + ' Hz';
+    });
+});
+
+function updatePESTOM(f0) {
+    if (pestomdata === null) {
+        return;
+    }
+
+    var correlation = 0;
+    for (let n=0; n<pestomdata.spectrum.length; n++) {
+        correlation += pestomdata.spectrum[n] * pestomdata[f0][n];
+    }
+    var plotdata = [{
+        type: "scatter",
+        name: "template",
+        x: pestomdata.frequencies,
+        y: pestomdata[f0]
+    },
+    {
+        type: "scatter",
+        name: "spectrum",
+        x: pestomdata.frequencies,
+        y: pestomdata.spectrum
+    }];
+    var plotlayout = {
+        height: 200,
+        width: 650,
+        xaxis: { title: "frequency in Hz" },
+        yaxis: { range: [-0.3, 1], title: "amplitude" },
+        margin: { t:30, r:20, b:50, l:50 },
+        title: "Magnitude Correlation = " + correlation.toFixed(2)
+    };
+    Plotly.newPlot("pestom-visualization", plotdata, plotlayout);
+}
+
+updatePESTOM(150);
